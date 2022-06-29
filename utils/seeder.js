6c8bd34c8592bd397fd702f3e74c840f4a688876
mike <michaelwilsonsj@hotmail.com>
2022-06-28 22:19:21 -0400

    added seed data && started seeRooms data seeder

---
+++ utils/seeder.js
@@ -0,0 +1,14 @@
+import Room from "../models/room";
+import mongoose from "mongoose";
+import dbConnect from "../config/dbConnect";
+import rooms from '../data/rooms.json'
+
+dbConnect();
+
+const seedRooms = async () => {
+    try {
+
+    } catch (error) {
+
+    }
+}
